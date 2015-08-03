66b0a7551b607c1439608274a21492b4ac226dc8
Alexey Mazheika <mozh.by@gmail.com>
2015-08-03 15:34:30 +0300

    add unick check for session

--- statistics.php
+++ statistics.php
@@ -17,15 +17,31 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_VARIABLES);
 if (variable_get('statistics_count_content_views', 0) && variable_get('statistics_count_content_views_ajax', 0)) {
   $nid = $_POST['nid'];
   if (is_numeric($nid)) {
-	db_merge('node_counter')
-	  ->key(array('nid' => $nid))
-	  ->fields(array(
-		'daycount' => 1,
-		'totalcount' => 1,
-		'timestamp' => REQUEST_TIME,
-	  ))
-	  ->expression('daycount', 'daycount + 1')
-	  ->expression('totalcount', 'totalcount + 1')
-	  ->execute();
+		session_start();
+		$first_time = true;
+		if (isset($_SESSION['nodes'])){
+			$nodes = explode(',',$_SESSION['nodes']);
+			if (!in_array(arg(1),$nodes)){
+				$nodes[] = arg(1);
+			}else{
+				$first_time = false;
+			}
+		}else{
+			$nodes = array();
+		}
+		$_SESSION['nodes'] = implode(',',$nodes);
+	  
+		if ($first_time){
+			db_merge('node_counter')
+			  ->key(array('nid' => $nid))
+			  ->fields(array(
+				'daycount' => 1,
+				'totalcount' => 1,
+				'timestamp' => REQUEST_TIME,
+			  ))
+			  ->expression('daycount', 'daycount + 1')
+			  ->expression('totalcount', 'totalcount + 1')
+			  ->execute();
+		}
   }
 }
